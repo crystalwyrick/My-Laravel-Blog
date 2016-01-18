89f7abdc3a957d30372b3278cd726e86457ccc44
Crystal Wyrick <crystalwyrick@Crystals-MacBook-Pro.local>
2016-01-18 07:48:04 -0600

    added "next" and "previous" blog functionality - not working

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -2,12 +2,20 @@
 
 @section('content')
 
-<h1>{{$post->title}}</h1>
-<h3>{{$post->subtitle}}</h3>
-<p>{{$post->content}}</p>
-
-<p>Created: {{{$post->created_at->format('l, F jS Y @ h:i:s A')}}}</p>
-
-<p>Created: {{{$post->created_at->diffForHumans() }}}</p>
+<div class="box">
+	<h1>{{$post->title}}</h1>
+	<h3>{{$post->subtitle}}</h3>
+	<p>{{$post->content}}</p>
+	<p>Created: {{{$post->created_at->diffForHumans() }}}</p>
+	<button type="submit" class="btn btn-default">	
+		<a href="{{{ action('PostsController@edit', $post->id) }}}">Edit this post</a>
+	</button>
+	<button type="submit" class="btn btn-default">
+		<a href="{{ URL::to( 'users/' . $previous ) }}">Previous</a>
+	</button>
+	<button type="submit" class="btn btn-default">
+		<a href="{{ URL::to( 'users/' . $next ) }}">Next</a>
+	</button>
+</div>
 
 @stop
